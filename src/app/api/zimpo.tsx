33fc2039414d06d9aecb7a9bd9efdd1077e56c7e
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-09-27 12:47:33 -0300

    update fetch

--- src/app/api/zimpo.tsx
+++ src/app/api/zimpo.tsx
@@ -1,24 +1,15 @@
-let cachedData: null = null;
-let lastFetchTime = 0;
-const revalidateInterval = 3600 * 1000; // 3600 seconds (1 hour) in milliseconds
-
 export async function Zimpo() {
-  // Check if the data is cached and if it's time to revalidate
-  if (cachedData !== null && Date.now() - lastFetchTime < revalidateInterval) {
-    return cachedData;
-  }
+  // Gere um timestamp único para adicionar como parâmetro de consulta
+  const timestamp = Date.now();
+  const apiUrl = `https://tabelazimpo.onrender.com/?timestamp=${timestamp}`;
 
-  const response = await fetch("https://tabelazimpo.onrender.com");
+  const response = await fetch(apiUrl);
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
 
   const data = await response.json();
-
-  // Update the cache and last fetch time
-  cachedData = data;
-  lastFetchTime = Date.now();
 
   return data;
 }
