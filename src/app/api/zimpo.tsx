e8ff7d8c902a94a8cddf6020181a09b18ea1d5e5
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-09-26 21:27:54 -0300

    add revalidate

--- src/app/api/zimpo.tsx
+++ src/app/api/zimpo.tsx
@@ -1,9 +1,24 @@
-export async function Zimpo() {
-  const zimpoTable = await fetch("https://tabelazimpo.onrender.com");
+let cachedData: null = null;
+let lastFetchTime = 0;
+const revalidateInterval = 3600 * 1000; // 3600 seconds (1 hour) in milliseconds
 
-  if (!zimpoTable.ok) {
+export async function Zimpo() {
+  // Check if the data is cached and if it's time to revalidate
+  if (cachedData !== null && Date.now() - lastFetchTime < revalidateInterval) {
+    return cachedData;
+  }
+
+  const response = await fetch("https://tabelazimpo.onrender.com");
+
+  if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
 
-  return zimpoTable.json();
+  const data = await response.json();
+
+  // Update the cache and last fetch time
+  cachedData = data;
+  lastFetchTime = Date.now();
+
+  return data;
 }
